dda15041437789fdece55611bdfb2184399ee5b8
Назар Мазурик <nazarmazurik99@gmail.com>
2018-07-07 18:36:40 +0300

    Database setting up.

--- SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
+++ SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -1,11 +1,9 @@
-﻿
-using Moq;
-using Ninject;
+﻿using Ninject;
 using System;
 using System.Collections.Generic;
 using System.Web.Mvc;
 using SportsStore.Domain.Abstract;
-using SportsStore.Domain.Entities;
+using SportsStore.Domain.Concrete;
 
 namespace SportsStore.WebUI.Infrastructure
 {
@@ -21,15 +19,7 @@ namespace SportsStore.WebUI.Infrastructure
 
         private void AddBindings()
         {
-            Mock<IProductRepository> mock = new Mock<IProductRepository>();
-            mock.Setup(m => m.Products).Returns(new List<Product>
-            {
-                new Product(){Name = "Football",Price = 25},
-                new Product(){Name = "Surf board",Price = 179},
-                new Product(){Name = "Running shoes",Price = 95}
-            });
-
-            kernel.Bind<IProductRepository>().ToConstant(mock.Object);
+            kernel.Bind<IProductRepository>().To<EFProductRepository>();
         }
     }
 }
