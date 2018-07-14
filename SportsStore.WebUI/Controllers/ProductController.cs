6da6eeb18238c191968905c87fa08dbc0c5715d4
Назар Мазурик <nazarmazurik99@gmail.com>
2018-07-14 21:38:45 +0300

    Adding category filtering

--- SportsStore.WebUI/Controllers/ProductController.cs
+++ SportsStore.WebUI/Controllers/ProductController.cs
@@ -1,9 +1,6 @@
 ﻿using SportsStore.Domain.Abstract;
 using SportsStore.WebUI.Models;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace SportsStore.WebUI.Controllers
