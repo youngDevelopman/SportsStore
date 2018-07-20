bd3750719ee00432cfda37a6dcaaeee5ea6c3101
Назар Мазурик <nazarmazurik99@gmail.com>
2018-07-21 00:09:18 +0300

    Completting the cart controller.

--- SportsStore.WebUI/Controllers/CartController.cs
+++ SportsStore.WebUI/Controllers/CartController.cs
@@ -9,10 +9,11 @@ namespace SportsStore.WebUI.Controllers
     public class CartController : Controller
     {
         private IProductRepository repository;
-
-        public CartController(IProductRepository repo)
+        private IOrderProcessor orderProcessor;
+        public CartController(IProductRepository repo, IOrderProcessor proc)
         {
             repository = repo;
+            orderProcessor = proc;
         }
 
         public RedirectToRouteResult AddToCart(Cart cart, int productId, string returnUrl)
@@ -52,6 +53,25 @@ namespace SportsStore.WebUI.Controllers
         public ViewResult Checkout()
         {
             return View(new ShippingDetails());
+        }
+
+        [HttpPost]
+        public ViewResult Checkout(Cart cart, ShippingDetails shippingDetails)
+        {
+            if (cart.Lines.Count() == 0)
+            {
+                ModelState.AddModelError("", "Sorry, your cart is empty!");
+            }
+
+            if (ModelState.IsValid)
+            {
+                orderProcessor.ProcessOrder(cart, shippingDetails);
+                cart.Clear();
+                return View("Completed"); }
+            else
+            {
+                return View(shippingDetails);
+            }
         }
     }
 }
