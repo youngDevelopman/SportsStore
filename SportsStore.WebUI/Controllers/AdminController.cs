23d0cedf138b6d35a00170c7cfd2bcc23cbaa5e8
Назар Мазурик <nazarmazurik99@gmail.com>
2018-07-23 16:11:08 +0300

    Creating CRUD controller

---
+++ SportsStore.WebUI/Controllers/AdminController.cs
@@ -0,0 +1,24 @@
+﻿using SportsStore.Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class AdminController : Controller
+    {
+        private IProductRepository repository;
+
+        public AdminController(IProductRepository repo)
+        {
+            repository = repo;
+        }
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View(repository.Products);
+        }
+    }
+}
