c122d5e8d6e44339ff0310b1d588d7d5d3ab39c0
young_developman <nazarmazurik99@gmail.com>
2018-10-08 22:26:00 +0300

    Updating the product repository

--- SportsStore.Domain/Abstract/IProductRepository.cs
+++ SportsStore.Domain/Abstract/IProductRepository.cs
@@ -10,5 +10,7 @@ namespace SportsStore.Domain.Abstract
     public interface IProductRepository
     {
         IEnumerable<Product> Products { get; }
+
+        void SaveProduct(Product product);
     }
 }
