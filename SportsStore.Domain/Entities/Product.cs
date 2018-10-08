8759035cd3aa71c7c28d7c11c7553c6799441e3f
young_developman <nazarmazurik99@gmail.com>
2018-10-08 22:44:36 +0300

    Adding model validation

--- SportsStore.Domain/Entities/Product.cs
+++ SportsStore.Domain/Entities/Product.cs
@@ -9,9 +9,17 @@ namespace SportsStore.Domain.Entities
         public int ProductID { get; set; }
 
         [DataType(DataType.MultilineText)]
+        [Required(ErrorMessage ="Please enter a product name")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter a product description")]
         public string Description { get; set; }
+
+        [Required]
+        [Range(0.1,double.MaxValue,ErrorMessage ="Please enter a positive number")]
         public decimal Price { get; set; }
+
+        [Required(ErrorMessage = "Please enter a product category")]
         public string Category { get; set; }
     }
 }
